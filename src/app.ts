eaf8c37bff898d3ae1cc036681e54ad850c1fa5c
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-19 23:10:25 +0900

    [FIX]cors error

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ import bodyParser from "body-parser"
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger("combined"))
 app.use("/api", router)
-app.use(cors())
 
 export default app
