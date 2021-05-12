2e29af178e081232ebd587473048b3c90dda6f95
HongJunHyeok <edb1631@naver.com>
2021-05-12 19:56:56 +0900

    모든 포스트 조회 구현

--- src/api/v1/post/index.ts
+++ src/api/v1/post/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import createPost from "./post.ctrl/createPost";
 import deletePost from "./post.ctrl/deletePost";
 // import getPost from "./post.ctrl/getPost";
-// import getPosts from "./post.ctrl/getPosts";
+import getPosts from "./post.ctrl/getPosts";
 // import modifyPost from './post.ctrl/modifyPost'
 
 const router = Router();
 
 router.post("/", createPost);
 router.delete("/:idx", deletePost);
+router.get("/", getPosts);
+
 
 export default router;
--- src/api/v1/post/post.ctrl/getPosts.ts
+++ src/api/v1/post/post.ctrl/getPosts.ts
@@ -1,3 +1,24 @@
 import Post from "../../../../entity/Post";
 import { Request, Response } from "express";
 import * as logger from "../../../../lib/logger";
+import { getRepository } from "typeorm";
+
+export default async (req: Request, res: Response) => {
+  try {
+    const postRepo = getRepository(Post);
+    const posts = await postRepo.find();
+
+    return res.status(200).json({
+      message: "모든 포스트 조회 성공",
+      data: {
+        posts,
+        total: posts.length,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "서버 오류",
+      error,
+    });
+  }
+};
